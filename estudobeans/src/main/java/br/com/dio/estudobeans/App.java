f9c8869d79bb990d033362531217f7f533129394
rbverissimo <rverissimo.dev@gmail.com>
2023-01-25 12:05:04 -0300

    Criação da classe Livro e do ApplicationContext para manipular os beans

---
+++ estudobeans/src/main/java/br/com/dio/estudobeans/App.java
@@ -0,0 +1,12 @@
+package br.com.dio.estudobeans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+        ApplicationContext factory = new AnnotationConfigApplicationContext();
+    }
+
+}
