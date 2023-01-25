2942d98ff62e3592ee0a97683df49099b2cc4ae3
rbverissimo <rverissimo.dev@gmail.com>
2023-01-25 12:10:07 -0300

    Criada a classe de gerenciamento do bean de Livro, AppConfig passada como parâmetro no construtor da AnnotationConfigApplicationContext. No método main, a instancia da classe livro recebe um applicationContext usando o método getBean apotando para ela mesma.

--- estudobeans/src/main/java/br/com/dio/estudobeans/App.java
+++ estudobeans/src/main/java/br/com/dio/estudobeans/App.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
 
-        ApplicationContext factory = new AnnotationConfigApplicationContext();
+        ApplicationContext factory = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Livro livro = factory.getBean(Livro.class);
+        livro.exibir();
     }
 
 }
---
+++ estudobeans/src/main/java/br/com/dio/estudobeans/AppConfig.java
@@ -0,0 +1,15 @@
+package br.com.dio.estudobeans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Livro getLivro(){
+        return new Livro();
+    }
+    //A interpretação da annotation se fosse em XML seria tal qual a abaixo:
+    // <bean id="livro" class="br.com.dio.estudobeans.Livro"/>
+}
